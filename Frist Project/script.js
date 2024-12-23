fa084f9615ebe51d4b834b5fba0a86238a991fc8
Abdul Kadir <mabdulkadir5g@gmail.com>
2024-12-24 00:35:09 +0600

    update

--- Frist Project/script.js
+++ Frist Project/script.js
@@ -98,40 +98,90 @@ bmiSub.addEventListener('click', function () {
 });
 
 
-window.addEventListener('load', function () {
-    const loader = document.getElementById('loader');
-    const content = document.getElementById('content');
+//?IMG API
 
-    setTimeout(() => {
-        loader.style.display = 'none';
-    }, 1200);
-});
+const imgBoxes = document.querySelector('.img-boxes')
+const urlAPI = 'https://picsum.photos/v2/list';
+
+const getImg = ()=>{
+    fetch(urlAPI).then(data=>data.json()).then(items=>{
+        console.log(items);
+        const itemsEntries = Object.entries(items);
+        function randomItems(array){
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+              }
+              return array;
+        }
+        const randomFive = randomItems(itemsEntries).slice(0, 2);
+        console.log(randomFive);
 
 
-// ফাংশন যা সক্রিয় সেকশন হাইলাইট করে
+        // items.forEach(function(img){
+        //     const newBox = document.createElement('div');
+        //     newBox.classList.add('image-box');
+
+        //     const addImg = document.createElement('img');
+        //     addImg.src = img.download_url;
+        //     newBox.appendChild(addImg)
+            
+        //     const imgAuthor = document.createElement('h3');
+        //     imgAuthor.classList.add('img-author');
+        //     imgAuthor.textContent = img.author;
+        //     newBox.appendChild(imgAuthor)
+        //     imgBoxes.appendChild(newBox)
+        // })
+        randomFive.forEach(([key, value]) => {
+            const newBox = document.createElement('div');
+            newBox.classList.add('image-box');
+
+            const addImg = document.createElement('img');
+            addImg.src = value.download_url;
+            newBox.appendChild(addImg)
+            
+            const imgAuthor = document.createElement('h3');
+            imgAuthor.classList.add('img-author');
+            imgAuthor.textContent = value.author;
+            newBox.appendChild(imgAuthor)
+            imgBoxes.appendChild(newBox)
+          });
+    });
+}
+getImg()
+
+//?Loded
+// window.addEventListener('load', function () {
+//     const loader = document.getElementById('loader');
+//     const content = document.getElementById('content');
+
+//     setTimeout(() => {
+//         loader.style.display = 'none';
+//     }, 1200);
+// });
+
+
+//? Screen Light on off
 function highlightSection() {
     const sections = document.querySelectorAll(".section");
     const scrollPosition = window.scrollY;
   
     sections.forEach((section) => {
-      const sectionTop = section.offsetTop; // সেকশনের শুরুর পজিশন
+      const sectionTop = section.offsetTop; 
       const sectionHeight = section.offsetHeight;
-  
-      // স্ক্রল অবস্থান চেক করা
       if (
         scrollPosition >= sectionTop - sectionHeight / 3 &&
         scrollPosition < sectionTop + sectionHeight - sectionHeight / 3
-      ) {
-        section.classList.add("active"); // আলো জ্বালান
+      )
+       {
+        section.classList.add("active"); 
       } else {
-        section.classList.remove("active"); // আলো বন্ধ করুন
+        section.classList.remove("active"); 
       }
     });
   }
   
-  // স্ক্রল ইভেন্টে ফাংশন ট্রিগার করুন
   window.addEventListener("scroll", highlightSection);
-  
-  // পেজ লোড হলে প্রথমবারের জন্য হাইলাইট ফাংশন চালু করুন
+
   highlightSection();
   
--- Frist Project/scriptapi.js
+++ Frist Project/scriptapi.js
@@ -2,17 +2,22 @@ const quoteAuthor = document.querySelector('.random-quote #qouta-author');
 const quoteText = document.querySelector('.random-quote #quote-text');
 const quoteCat = document.querySelector('.random-quote #qouta-cat');
 const quoteButton = document.querySelector('.random-quote #get-quote');
-
-const APIUrl = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
+const catQuotes = ["age", "alone", "amazing", "anger", "architecture", "art", "attitude", "beauty", "best", "birthday", "business", "car", "change", "communication", "computers", "cool", "courage", "dad", "dating", "death", "design", "dreams", "education", "environmental", "equality", "experience", "failure", "faith", "family", "famous", "fear", "fitness", "food", "forgiveness", "freedom", "friendship", "funny", "future", "god", "good", "government", "graduation", "great", "happiness", "health", "history", "home", "hope", "humor", "imagination", "inspirational", "intelligence", "jealousy", "knowledge", "leadership", "learning", "legal", "life", "love", "marriage", "medical", "men", "mom", "money", "morning", "movies", "success"];
 
 const getQuote = ()=>{
-    fetch(APIUrl,{headers: { 'X-Api-Key': 'aMwFpjSOFYAFGHNz+PwTuQ==5i0EKevTZRt8wBsy'},}).then(data=>data.json()).then(item=>{
+      
+    const catIndex = Math.floor(Math.random()*catQuotes.length);
+    const catQuote = catQuotes[catIndex];
+    const APIUrl = 'https://api.api-ninjas.com/v1/quotes?category='+catQuote;
 
+    fetch(APIUrl,{headers: { 'X-Api-Key': 'aMwFpjSOFYAFGHNz+PwTuQ==5i0EKevTZRt8wBsy'},}).then(data=>data.json()).then(item=>{
         console.log(item);
         quoteText.innerHTML = '<q>'+item[0].quote+'</q>';
         quoteAuthor.innerHTML = '― '+item[0].author;
         quoteCat.innerHTML ='Category : '+ item[0].category;
-    });
+    }).catch(error => {
+        console.error('Error Fatching Quotes', error);
+      });
 
 }
 getQuote();
